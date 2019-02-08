e327fe93c48f056681438bf182cd2bbfdf143672
william <william.walder@gmail.com>
2019-02-08 00:08:39 +0000

    Criação de rota DELETE deleteByName

---
+++ src/main/java/com/javaee/jersey/services/VehicleService.java
@@ -0,0 +1,18 @@
+package com.javaee.jersey.services;
+
+import java.util.List;
+
+import com.javaee.jersey.domain.Vehicle;
+
+public interface VehicleService {
+
+	List<Vehicle> getAll();
+
+	Vehicle findById(Integer id);
+
+	Vehicle saveVehicle(Vehicle vehicle);
+
+	void deleteById(Integer id);
+	
+	void deleteByName(String name);
+}
